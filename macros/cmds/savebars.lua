9ac2785ceca38e72e454dbda11ca885186fbd4ee
Guilherme Caulada <guilherme.caulada@gmail.com>
2024-08-10 15:58:37 -0300

    Fix issue saving macros on savebars

--- macros/cmds/savebars.lua
+++ macros/cmds/savebars.lua
@@ -1,3 +1,3 @@
 #cmd savebars
 #run {|SAVE|} {|LOUT|}
-local b={} for i=1,240 do local a={GetActionInfo(i)} if a[1] then b[i]=a end end _S(b, "sb",_LO.spec, _LO.name)
+local b={} for i=1,240 do local a={GetActionInfo(i)} if a[1] then b[i]=a if a[1] == "macro" then b[i][2]=GetActionText(i) end end _S(b, "sb",_LO.spec, _LO.name)

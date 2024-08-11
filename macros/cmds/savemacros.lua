c884f5d8f1b1c836edc6d2c0c8e564619405468f
Guilherme Caulada <guilherme.caulada@gmail.com>
2024-08-11 13:03:54 -0300

    Fix icon issue when saving showtooltip macros

--- macros/cmds/savemacros.lua
+++ macros/cmds/savemacros.lua
@@ -1,3 +1,3 @@
 #cmd savemacros
 #run {|SAVE|} {|LOUT|}
-for i=121,138 do local m={GetMacroInfo(i)} if m[1] then _S(m,"sm",_LO.class,i) end end
+for i=121,138 do local m={GetMacroInfo(i)} if m[1] then if strfind(m[3], "#showtooltip") then m[2]=134400 end _S(m,"sm",_LO.class,i) end end
